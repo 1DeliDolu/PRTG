30f039067ad51d9a7e20c91170fe6fdc5e24b85e
Mustafa Özdemir <mustafa.ozdemir1408@gmail.com>
2025-02-16 12:39:45 +0100

    path

--- maxmarkusprogram-prtg-datasource/src/components/ConfigEditor.tsx
+++ maxmarkusprogram-prtg-datasource/src/components/ConfigEditor.tsx
@@ -60,7 +60,7 @@ export function ConfigEditor(props: Props) {
           id="config-editor-path"
           onChange={onPathChange}
           value={jsonData.path}
-          placeholder="Enter the path, e.g. /api/v1"
+          placeholder="Enter the path, e.g. https://prtg.example"
           width={60}
         />
       </InlineField>
